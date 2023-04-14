22325a129ebea1ac9ce2926e66413d7976ee3f17
e-juhee <juhee971204@gmail.com>
2023-04-14 14:02:19 +0000

    Feat: [#1] tiny webserver

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,7 +4,34 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) {
-  exit(0);
+int main(void)
+{
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1 = 0, n2 = 0;
+
+    if ((buf = getenv("QUERY_STRING")) != NULL)
+    {
+        p = strchr(buf, "&");
+        *p = '\0';
+        strcpy(arg1, buf);
+        strcpy(arg2, p + 1);
+        n1 = atoi(arg1);
+        n2 = atoi(arg2);
+    }
+
+    sprintf(content, "QUERY_STRING=%s", buf); // HTML 형식으로 생성
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks for visiting!\r\n", content);
+
+    printf("Connection: close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content); // HTML 응답의 바디인 content 출력
+    fflush(stdout);        // 출력 버퍼 비움
+
+    exit(0);
 }
 /* $end adder */
